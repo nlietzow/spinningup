3dbd3c3451cc7b083cc037f9dc36177097a4a541
Niclas Lietzow <niclas.lietzow@outlook.com>
2025-02-19 23:29:24 +0100

    rm dead code from logger

--- src/utils/logx.py
+++ src/utils/logx.py
@@ -65,44 +65,19 @@ def statistics_scalar(x, with_min_and_max=False):
 
 def setup_logger_kwargs(exp_name, seed=None, data_dir=None, datestamp=False):
     """
-    Sets up the output_dir for a logger and returns a dict for logger kwargs.
-
-    If no seed is given and datestamp is false,
-
-    ::
-
-        output_dir = data_dir/exp_name
-
-    If a seed is given and datestamp is false,
-
-    ::
-
-        output_dir = data_dir/exp_name/exp_name_s[seed]
-
-    If datestamp is true, amend to
-
-    ::
-
-        output_dir = data_dir/YY-MM-DD_exp_name/YY-MM-DD_HH-MM-SS_exp_name_s[seed]
-
-    You can force datestamp=True by setting ``FORCE_DATESTAMP=True`` in
-    ``spinup/user_config.py``.
+    Sets up an output directory for logging and returns logger keyword arguments.
 
     Args:
-
         exp_name (string): Name for experiment.
-
         seed (int): Seed for random number generators used by experiment.
-
         data_dir (string): Path to folder where results should be saved.
-            Default is the ``DEFAULT_DATA_DIR`` in ``spinup/user_config.py``.
-
+            Default is None, which means use the DEFAULT_DATA_DIR.
         datestamp (bool): Whether to include a date and timestamp in the
             name of the save directory.
 
     Returns:
-
-        logger_kwargs, a dict containing output_dir and exp_name.
+        logger_kwargs (dict): Dictionary containing output directory and
+            experiment name, to be passed to the logger class constructors.
     """
 
     # Datestamp forcing
@@ -390,15 +365,3 @@ class EpochLogger(Logger):
                 super().log_tabular("Max" + key, stats[3])
                 super().log_tabular("Min" + key, stats[2])
         self.epoch_dict[key] = []
-
-    def get_stats(self, key):
-        """
-        Lets an algorithm ask the logger for mean/std/min/max of a diagnostic.
-        """
-        v = self.epoch_dict[key]
-        vals = (
-            np.concatenate(v)
-            if isinstance(v[0], np.ndarray) and len(v[0].shape) > 0
-            else v
-        )
-        return statistics_scalar(vals)
