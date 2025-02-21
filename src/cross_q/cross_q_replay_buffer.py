733c647bdd58145f40efb7b406a7b2fdac6141cd
Niclas Lietzow <niclas.lietzow@outlook.com>
2025-02-21 13:03:23 +0100

    add load and save to buffer

--- src/cross_q/cross_q_replay_buffer.py
+++ src/cross_q/cross_q_replay_buffer.py
@@ -1,7 +1,9 @@
+from pathlib import Path
 from typing import NamedTuple, Optional, SupportsFloat, Union
 
 import numpy as np
 import torch
+from tensordict import TensorDict
 
 
 class Batch(NamedTuple):
@@ -21,29 +23,33 @@ class ReplayBuffer:
         self,
         obs_dim: tuple[int, ...],
         act_dim: int,
-        size: int,
+        max_size: int,
         device: torch.device,
     ):
         # Initialize tensors directly on the specified device
         self.obs_buf, self.obs2_buf = (
             torch.zeros(
-                self.combined_shape(size, obs_dim),
+                self.combined_shape(max_size, obs_dim),
                 dtype=torch.float32,
                 device=device,
             )
             for _ in range(2)
         )
         self.act_buf = torch.zeros(
-            self.combined_shape(size, act_dim),
+            self.combined_shape(max_size, act_dim),
             dtype=torch.float32,
             device=device,
         )
         self.rew_buf, self.done_buf = (
-            torch.zeros(size, dtype=torch.float32, device=device) for _ in range(2)
+            torch.zeros(max_size, dtype=torch.float32, device=device) for _ in range(2)
         )
 
-        self.ptr, self.size, self.max_size = 0, 0, size
+        self.ptr, self.size = 0, 0
         self.device = device
+
+    @property
+    def max_size(self) -> int:
+        return self.obs_buf.shape[0]
 
     def to_tensor(self, array: np.ndarray) -> torch.Tensor:
         return torch.as_tensor(array, dtype=torch.float32, device=self.device)
@@ -89,3 +95,39 @@ class ReplayBuffer:
             return (length, shape)
 
         return (length, *shape)
+
+    def save(self, path: Path) -> None:
+        torch.save(
+            TensorDict(
+                {
+                    "obs_buf": self.obs_buf.cpu(),
+                    "obs2_buf": self.obs2_buf.cpu(),
+                    "act_buf": self.act_buf.cpu(),
+                    "rew_buf": self.rew_buf.cpu(),
+                    "done_buf": self.done_buf.cpu(),
+                    "ptr": torch.tensor(self.ptr, device="cpu"),
+                    "size": torch.tensor(self.size, device="cpu"),
+                    "max_size": torch.tensor(self.max_size, device="cpu"),
+                },
+                batch_size=[],
+            ),
+            path,
+        )
+
+    @classmethod
+    def load(cls, path: Path, device: torch.device) -> "ReplayBuffer":
+        data = torch.load(path)
+        buffer = cls(
+            obs_dim=data["obs_buf"].shape[1:],
+            act_dim=data["act_buf"].shape[1:],
+            max_size=data["max_size"].item(),
+            device=device,
+        )
+        buffer.obs_buf = data["obs_buf"].to(device)
+        buffer.obs2_buf = data["obs2_buf"].to(device)
+        buffer.act_buf = data["act_buf"].to(device)
+        buffer.rew_buf = data["rew_buf"].to(device)
+        buffer.done_buf = data["done_buf"].to(device)
+        buffer.ptr = data["ptr"].item()
+        buffer.size = data["size"].item()
+        return buffer
